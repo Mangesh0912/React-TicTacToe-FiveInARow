040cf1162aa4447938217b7280c5e2db1d781b70
Mangesh <mangesh091287@gmail.com>
2020-06-18 01:21:02 +0530

    React Tic Tac Toe Code Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Board from './components/Board'
+import * as utils from './utils/functions'
+import './styles/board.css'
+import './styles/box.css'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Board></Board>
+      </div>
   );
 }
 
 export default App;
+
+
